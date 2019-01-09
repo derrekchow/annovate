e541c439eef59439b82a2d9f58b28f4d25ad5efc
Matt Bonnell <matt.bonnell@icloud.com>
2019-01-09 17:21:10 -0500

    Created app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = new annotator.App();
+app.include(annotator.ui.main, {
+    editorExtensions: [annotator.ui.tags.editorExtension],
+    viewerExtensions: [
+        annotator.ui.markdown.viewerExtension,
+        annotator.ui.tags.viewerExtension
+    ]
+});
+app.include(annotator.storage.debug);
+app.start();
