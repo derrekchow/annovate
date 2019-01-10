b1e5719173c8fd075736d2b194fd0a67da4fed82
derrekchow <derrekdc@gmail.com>
2019-01-10 13:12:12 -0500

    GET and POST working

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const bodyParser = require('body-parser')
 const port = 3000
 
+var annotation = { quote:'ansistors are made from very pure silicon or germanium, but certain other semiconductor materials can also be used. A transistor may have only one kind of charge carrier, in a field effect transistor, or may have two kinds of charge carriers in bipolar junction transistor devices.',ranges:[ { start: '', startOffset: 1360, end: '', endOffset: 1642 } ],text: 'asdfasdf',tags: [] }
+var annotations = {"total": 1,"rows": [annotation]}
+
 app.use(bodyParser.urlencoded({
  	extended: true
 }));
@@ -12,17 +15,18 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
 
-app.route('/api/annotations')
-	.get((req, res) => {
-		console.log("GET request")
-		res.send("GET")
-	})
-	.post((req, res) => {
-		console.log("POST " + req.url)
-		console.log(req.body)
-		res.type("json")
-		res.json({name: "Joe Smith"})
-	})
+app.post('/api/annotations', (req, res) => {
+	console.log("POST REQUEST: " + req.url)
+	console.log(req.body)
+	res.type("json")
+	res.json(req.body)
+})
+
+app.get('/api/search', (req, res) => {
+	console.log("GET SEARCH")
+	res.type("json")
+	res.json(annotations)
+})
 
 app.use(express.static(__dirname + '/public'))
 
--- public/script.js
+++ public/script.js
@@ -1,11 +1,17 @@
 var app = new annotator.App();
+
+app.include(annotator.storage.http, {
+	prefix: '/api'
+});
+
 app.include(annotator.ui.main, {
     editorExtensions: [annotator.ui.tags.editorExtension],
     viewerExtensions: [
-        annotator.ui.markdown.viewerExtension,
         annotator.ui.tags.viewerExtension
     ]
 });
-app.include(annotator.storage.debug);
 
-app.start();
+app.start()
+.then(() => {
+	app.annotations.load()
+})
