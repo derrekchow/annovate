c375a4445c89991d7739b09e9741be51b8ae0520
Matt Bonnell <matt.bonnell@icloud.com>
2019-01-10 16:52:43 -0500

    Changes to annotator.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const Annotation = require('../models/annotation')
 
-var annotation = { quote:'ansistors are made from very pure silicon or germanium, but certain other semiconductor materials can also be used. A transistor may have only one kind of charge carrier, in a field effect transistor, or may have two kinds of charge carriers in bipolar junction transistor devices.',ranges:[ { start: '', startOffset: 1360, end: '', endOffset: 1642 } ],text: 'asdfasdf',tags: [] }
-var annotations = {"total": 1,"rows": [annotation]}
+var annotation1 = { quote:'ansistors are made from very pure silicon or germanium, but certain other semiconductor materials can also be used. A transistor may have only one kind of charge carrier, in a field effect transistor, or may have two kinds of charge carriers in bipolar junction transistor devices.',ranges:[ { start: '', startOffset: 1360, end: '', endOffset: 1642 } ],text: 'asdfasdf',tags: ['Unclear'] }
+var annotation2 = { quote: 'ction to an external cir',
+ranges: [ { start: '', startOffset: 197, end: '', endOffset: 221 } ],
+text: '', tags: ['Clear']  }
+var annotations = {"total": 2,"rows": [annotation1, annotation2]}
 
 router.post('/api/annotations', (req, res) => {
 	console.log("POST REQUEST: " + req.url)
