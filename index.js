85e5d51e83f6276dfa25e7213e0ce71355c74d20
derrekchow <derrekdc@gmail.com>
2019-01-10 11:12:02 -0500

    created endpoints

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
+
+app.use(bodyParser.urlencoded({
+ 	extended: true
+}));
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
 
+app.route('/api/annotations')
+	.get((req, res) => {
+		console.log("GET request")
+		res.send("GET")
+	})
+	.post((req, res) => {
+		console.log("POST " + req.url)
+		console.log(req.body)
+		res.type("json")
+		res.json({name: "Joe Smith"})
+	})
+
 app.use(express.static(__dirname + '/public'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
