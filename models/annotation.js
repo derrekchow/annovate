326b71aab3c5590deaf551145e571f291f771636
derrekchow <derrekdc@gmail.com>
2019-01-11 11:32:47 -0500

    data to and from db

--- models/annotation.js
+++ models/annotation.js
@@ -1,17 +1,47 @@
-//const db = require('./db')
+const db = require('./db')
 
-exports.save = (req, cb) => {
-	var sql = "INSERT INTO annotations (pageName, reaction, content) values (" + JSON.stringify(req.pageName) + ", " + JSON.stringify(req.reaction) + ", " + JSON.stringify(req) + ")"
-	db.query(sql, (err, rows) => {
-		if(err) throw Error()
-		cb(req)
+function createAnnotation(item) {
+	var ann = {}
+	ann.uid = item.uid
+	ann.aid = item.aid
+	ann.quote = item.quote
+	ann.ranges = [{ start: '', startOffset: item.startOffset, end: '', endOffset: item.endOffset }]
+	ann.tags = [ item.tag ]
+	ann.text = ''
+	return ann
+}
+
+exports.save = (ann, cb) => {
+	var sql = "INSERT INTO annotations (page, uid, aid, tag, startOffset, endOffset, quote) VALUES (?, ?, ?, ?, ?)";
+	var values = [
+		"se212",
+		ann.uid,
+		ann.id,
+		ann.tags[0],
+		ann.ranges[0].startOffset,
+		ann.ranges[0].endOffset,
+		ann.quote
+	]
+	db.query(sql, values, (err) => {
+		if(err) throw Error(err)
+		console.log("Annotation added to db: ", ann)
+		cb(ann)
 	})
 }
 
-exports.getAll = (cb) => {
-	db.query('SELECT * FROM annotations', (err, rows) => {
+exports.get = (uid, cb) => {
+	var sql = "SELECT * FROM annotations"
+	if(uid != undefined) {
+		sql += " WHERE uid=" + uid
+	}
+	db.query(sql, (err, rows) => {
 		if(err) throw Error()
-		var result = { "total": rows.length, "rows": rows }
+		var data = []
+		for (var item of rows) {
+			data.push(createAnnotation(item))
+		}
+		var result = { "total": rows.length, "rows": data }
+		console.log("Retrieved annotations: ", data)
 		cb(result) // TODO: contstruct the correct array with annotation objects by fetching from db
 	})
 }
